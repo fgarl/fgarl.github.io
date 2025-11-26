b5466d4897e72e0b776291adcc9374a7113a10f8
Fernando García <fdogarcialagos@gmail.com>
2025-11-26 13:04:56 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+	[
+		{ 
+			name: "Servo",
+			page: "arduino.html",
+		},
+		{ 
+			name: "Vis",
+			page: "index.html",
+			main: true,
+		}
+	]);
